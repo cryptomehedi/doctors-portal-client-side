2374c14a3aa0667d66dbb5232944f5013962d780
cryptomehedi <classicalmehedi@gmail.com>
2022-05-10 01:24:42 +0600

    Nav bar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Component/Pages/Home/Home';
+import Nav from './Component/Pages/Shared/Nav';
 
 function App() {
   return (
     <div>
-      
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
     </div>
   );
 }
