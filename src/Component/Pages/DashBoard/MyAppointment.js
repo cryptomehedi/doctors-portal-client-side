f712256ab2152957f2228ddf7062bd2f691d7882
cryptomehedi <classicalmehedi@gmail.com>
2022-05-15 20:40:55 +0600

    MyAppointment

--- src/Component/Pages/DashBoard/MyAppointment.js
+++ src/Component/Pages/DashBoard/MyAppointment.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { TrashIcon } from '@heroicons/react/solid'
 
 const MyAppointment = () => {
     const [appointments, setAppointments] =useState([])
@@ -19,6 +20,32 @@ const MyAppointment = () => {
     return (
         <div>
             <h2>MyAppointment: {appointments.length}</h2>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                    <tr>
+                        <th>SN.</th>
+                        <th>Treatment Name</th>
+                        <th>Time</th>
+                        <th>Date</th>
+                        <th>Cancel</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appointments.map((a,i)=>  <tr class="hover">
+                            <th>{i+1}</th>
+                            <td>{a.treatment}</td>
+                            <td>{a.slot}</td>
+                            <td>{a.date}</td>
+                            <td> <div className=' text-red-400 w-8 h-8 bg-accent cursor-pointer flex justify-center items-center rounded-full'><div className='w-5'>{<TrashIcon/>}</div></div></td>
+
+                        </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
